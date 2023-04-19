c6ab558f8fef4910a6533920dbd797e9544cc347
Naya070 <nayarith070@gmail.com>
2023-04-19 15:00:45 -0400

    se agrego interfaz de usuario ui mas otras clases

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,25 +1,39 @@
 public class Doctor {
     //Estos son los atributos o propiedades de la clase Doctor.
-    int id;
-    String name;
-    String speciality;
+    //int id;
+    //String name;
+    //String speciality;
     //Método constructor.
     //debe tener el nombre de la clase en la mayoría de los casos.
     //No retorna ningún valor.
     //Usa cero o más argumentos contenidos dentro de los
     // paréntesis que siguen al nombre.
 
-    //Aquí hay dos opciones para construir objetos de la clase Doctor.
-    Doctor(){
-        System.out.println("Construyendo el objeto doctor");
-    }
-    Doctor(String name){
-        System.out.println("El nombre del doctor asignado es: " + name);
+        //Atributos
+        static int id = 0; //Autoincrement
+        String name;
+        String speciality;
+
+        Doctor(){
+            System.out.println("Construyendo el Objeto Doctor");
+
+        }
+
+        Doctor(String name, String speciality){
+            System.out.println("El nombre del Doctor asignado es: " + name);
+            id++;
+            this.name = name;
+            this.speciality = speciality;
+        }
+
+        //Comportamientos
+        public void showName(){
+            System.out.println(name);
+        }
+
+        public void showId(){
+            System.out.println("ID Doctor: " + id);
+        }
+
     }
 
-    //comportamientos
-    public void showName(){
-        System.out.println(name);
-
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor1 = new Doctor();
-        Doctor myDoctor2 = new Doctor("Nayarith Jiménez");
+        //Doctor myDoctor1 = new Doctor( );
+        //Doctor myDoctor2 = new Doctor("Nayarith Jiménez");
         // Doctor() es el métod constructor.
         //crea nuevas instancias de una clase.
         //Usa la palabra new para invocarlo.
-        myDoctor2.name = "Joan Blanco";
-        myDoctor2.showName();
+        //myDoctor2.name = "Joan Blanco";
+        //myDoctor2.showName();
+
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 }
